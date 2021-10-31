19688503beb4d81e20fe76c93d99f50b5c31a01a
Do Huy Hoang <hoangdohuy29092002@gmail.com>
2021-11-01 00:05:00 +0700

    31/10/2021: hoan thien giao dien insert laptop

--- src/com/controller/InsertLaptopImageController.java
+++ src/com/controller/InsertLaptopImageController.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.model.Laptop;
 import com.view.Navigator;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 public class InsertLaptopImageController implements Initializable {
   @FXML
@@ -66,7 +68,19 @@ public class InsertLaptopImageController implements Initializable {
   private HBox imgChoosedLayout;
 
   @FXML
+  private Button btnPrevious;
+
+  @FXML
   private Button btnNext;
+
+  @FXML
+  void clickChooseImage(MouseEvent mouseEvent) {
+    List<File> listFile = fileChooser.showOpenMultipleDialog(Navigator.getInstance().getStage());
+    setImgChoosed(listFile);
+    for(File i : listFile) {
+      System.out.println(i);
+    }
+  }
 
   @FXML
   private void clickNext() throws IOException {
@@ -77,8 +91,6 @@ public class InsertLaptopImageController implements Initializable {
   private void clickPrevious() throws IOException {
     Navigator.getInstance().goToInsertLaptopBasicInfo();
   }
-
-  int count;
 
   @FXML
   void showAddProductMousePressed(MouseEvent event) {
@@ -102,53 +114,27 @@ public class InsertLaptopImageController implements Initializable {
     }
   }
 
-  @FXML
-  void clickChooseImage(ActionEvent event) {
-
-  }
-
-//  private List<Laptop> list;
+  int count;
+  FileChooser fileChooser = new FileChooser();
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-//    list = new ArrayList<>(list());
 
-//    for (int i = 0; i < list.size(); i++) {
-//      try {
-//        FXMLLoader fxmlLoader = new FXMLLoader();
-//        fxmlLoader.setLocation(getClass().getResource("/com/view/imgChoosedUI.fxml"));
-//        HBox imgBox = fxmlLoader.load();
-//        ImgChoosedController imgChoosedController = fxmlLoader.getController();
-//        imgChoosedController.setData(list.get(i).getImgSrc());
-//        imgChoosedLayout.getChildren().add(imgBox);
-//      } catch (IOException e) {
-//        e.printStackTrace();
-//      }
-//    }
   }
 
-//  private List<Laptop> list() {
-//    List<Laptop> listAdd = new ArrayList<>();
-//    Laptop laptop = new Laptop();
-//    laptop.setImgSrc("/com/images/iphone-13-pro-max-bac.jpg");
-//    listAdd.add(laptop);
-//
-//    laptop = new Laptop();
-//    laptop.setImgSrc("/com/images/iphone-13-pro-max-bac.jpg");
-//    listAdd.add(laptop);
-//
-//    laptop = new Laptop();
-//    laptop.setImgSrc("/com/images/iphone-13-pro-max-bac.jpg");
-//    listAdd.add(laptop);
-//
-//    laptop = new Laptop();
-//    laptop.setImgSrc("/com/images/iphone-13-pro-max-bac.jpg");
-//    listAdd.add(laptop);
-//
-//    laptop = new Laptop();
-//    laptop.setImgSrc("/com/images/iphone-13-pro-max-bac.jpg");
-//    listAdd.add(laptop);
-//
-//    return listAdd;
-//  }
+  public void setImgChoosed(List<File> list) {
+    for (int i = 0; i < list.size(); i++) {
+      try {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/com/view/imgChoosedUI.fxml"));
+        HBox imgBox = fxmlLoader.load();
+        ImgChoosedController imgChoosedController = fxmlLoader.getController();
+        System.out.println(list.get(i));
+        imgChoosedController.setData(list.get(i).toString());
+        imgChoosedLayout.getChildren().add(imgBox);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
